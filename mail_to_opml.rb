17a8db8546cd14b208d9799c88028668b2adbabc
Benjamin S. Rhodes <benjamin.s.rhodes@gmail.com>
2010-07-05 18:17:19 -0400

    First commit

---
+++ mail_to_opml.rb
@@ -0,0 +1,51 @@
+require 'builder'
+
+feeds = []
+rss_dir = "#{ENV['HOME']}/Library/Mail/RSS"
+files = Dir.glob(rss_dir + "/*.rssmbox")
+
+files.each do |file|
+  feed = {}
+  
+  if file =~ /.*\/(.*).rssmbox/
+    feed[:name] = $1
+  end
+  next_line_is_value = false
+  
+  plist = File.new(file + "/Info.plist", "r")
+  while(line = plist.gets)
+    if next_line_is_value
+      if line =~ /<string>(.*)<\/string>/
+        feed[:url] = $1
+      end
+      next_line_is_value = false
+    end
+    
+    if "<key>RSSFeedURLString</key>" == line.strip
+      next_line_is_value = true
+    end
+  end
+  
+  feeds << feed
+end
+
+def feed_t_xml(feed, builder)
+  builder.outline(
+    :text     => feed[:name], 
+    :title    => feed[:name], 
+    :htmlUrl  => feed[:url], 
+    :xmlUrl   => feed[:url],
+    :type     => 'rss', )
+end
+
+x = Builder::XmlMarkup.new(:target => $stdout, :indent => 1)
+x.instruct!
+x.opml {
+  x.body {
+    feeds.each do |f|
+      feed_t_xml(f, x)
+    end
+  }
+}
+
+
